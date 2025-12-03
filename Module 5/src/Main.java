9c1d1fff1b37afd3df4a24a015d42039e479f02d
angelicabasa <angelicanicole.basa@benilde.edu.ph>
2025-12-04 02:09:39 +0800

    Quiz 4 Answers

---
+++ Module 5/src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    Player ploo = new Player (134, "Plooful", 135);
+    Player wardell = new Player (536, "TSM Wardell", 640);
+    Player deadlyJimmy = new Player (32, "DeadlyJimmy", 34);
+    Player subroza = new Player (4931, "Subroza", 604);
+    Player annieDro = new Player (6919, "C9 Annie", 593);
+
+    SimpleHashTable hashtable = new  SimpleHashTable();
+    hashtable.put(ploo.getUserName(), ploo);
+    hashtable.put(wardell.getUserName(), wardell);
+    hashtable.put(deadlyJimmy.getUserName(), deadlyJimmy);
+    hashtable.put(subroza.getUserName(), subroza);
+    hashtable.put(annieDro.getUserName(), annieDro);
+
+    hashtable.printHashtable();
+    System.out.println("\n--------------------------");
+    System.out.println("\nREMOVE PLAYER: SUBROZA");
+    hashtable.remove(subroza.getUserName());
+    System.out.println("\nHashtable after removing: ");
+    hashtable.printHashtable();
+
+       // the key is the player's username
+//        System.out.println(hashtable.get("Subroza"));
+    }
+}
---
+++ Module 5/src/SimpleHashTable.java
@@ -0,0 +1,115 @@
+public class SimpleHashTable {
+    private StoredPlayer[] hashtable;
+
+    public SimpleHashTable() {
+        hashtable = new StoredPlayer[10];
+    }
+
+    private int hashKey(String key) {
+        return key.length() % hashtable.length;
+    }
+
+    public void put(String key, Player value) {
+        int hashedKey = hashKey(key);
+
+        if (isOccupied(hashedKey)) {
+            // do linear probing
+            int stoppingIndex = hashedKey;
+
+            if (hashedKey == hashtable.length - 1) {
+                hashedKey = 0;
+            }
+            else {
+                hashedKey++;
+            }
+
+            while (isOccupied(hashedKey) && hashedKey != stoppingIndex) {
+                hashedKey = (hashedKey + 1) % hashtable.length;
+            }
+        }
+
+        if (isOccupied(hashedKey)) {
+            System.out.println("Sorry, there is already an element at position " + hashedKey);
+        }
+        else {
+            hashtable[hashedKey] = new StoredPlayer(key, value);
+        }
+    }
+
+    private boolean isOccupied(int index) {
+        return hashtable[index] != null;
+    }
+
+    public Player get(String key) {
+        int hashedKey = findKey(key);
+
+        if (hashedKey == -1) {
+            return null;
+        }
+        return hashtable[hashedKey].value;
+    }
+
+    private int findKey(String key) {
+        int hashedKey = hashKey(key);
+
+        // found the right key?
+        if (hashtable[hashedKey] != null && hashtable[hashedKey].key.equals(key)) {
+            return hashedKey;
+        }
+            // do linear probing
+            int stoppingIndex = hashedKey;
+
+            if (hashedKey == hashtable.length - 1) {
+                hashedKey = 0;
+            }
+            else {
+                hashedKey++;
+            }
+
+            while (hashedKey != stoppingIndex && hashtable[hashedKey] != null &&
+                    !hashtable[hashedKey].key.equals(key)) {
+                hashedKey = (hashedKey + 1) % hashtable.length;
+            }
+
+            if (hashtable[hashedKey] != null
+            && hashtable[hashedKey].key.equals(key)) {
+                return hashedKey;
+            }
+    return -1;
+    }
+
+    public Player remove(String key) {
+        int hashedKey = findKey(key);
+
+        if (hashedKey == -1) {
+            return null;
+        }
+
+        Player removedPlayer =  hashtable[hashedKey].value;
+        hashtable[hashedKey] = null;
+
+        StoredPlayer[] previousHashTable = hashtable;
+        hashtable = new StoredPlayer[previousHashTable.length];
+
+        for (StoredPlayer storedPlayer : previousHashTable) {
+            if (storedPlayer != null) {
+                put(storedPlayer.key, storedPlayer.value);
+            }
+        }
+        return removedPlayer;
+    }
+
+    public void printHashtable()
+    {
+        for (int i = 0; i < hashtable.length; i++)
+        {
+            if (hashtable[i] != null) {
+                System.out.println("Element " + i + " " + hashtable[i].value);
+            }
+            else
+            {
+                System.out.println("Element " + i + " null");
+            }
+        }
+    }
+}
